b1993302bba0cc5b8fa9991978b125ae2bf76b18
kurtwall <kwall@kurtwerks.com>
2023-11-10 21:17:16 -0800

    Check-in after completing part 7 of the tutorial

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -14,6 +15,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?"
+    )
     def was_published_recently(self):
         """
         Return true if poll published in the past day
